a197448238b6a9272779f2347cd9ce708aafaf0f
Ivan Rusanov <ivan.b.rusanov@gmail.com>
2019-09-23 01:00:04 +0300

    Change version to beta

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,3 +29,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
 [assembly: AssemblyVersion("0.8.0.*")]
+[assembly: AssemblyInformationalVersion("0.8.0-beta")]
